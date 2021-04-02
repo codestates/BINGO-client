c52215a3620e73296837bfa60763e67b81b54c1a
JunghoChoi <9rganizedchaos@gmail.com>
2021-04-02 15:40:02 +0900

    Fix App.js for testing deploy process

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div>Hello, World</div>;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          const divBox = document.querySelector(".divBox");
+          axios.get("server.ibingo.link").then((res) => {
+            divBox.textContent = res.data;
+          });
+        }}
+      >
+        인사하겠습니다
+      </button>
+      <div className="divBox"></div>
+    </div>
+  );
 }
 
 export default App;
