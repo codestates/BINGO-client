cf8303b5c74b0667524759996cd67080bab6d628
JunghoChoi <9rganizedchaos@gmail.com>
2021-04-05 15:17:16 +0900

    Fix some more setup files

--- src/pages/GuidePage.tsx
+++ src/pages/GuidePage.tsx
@@ -1,24 +1,14 @@
-import React, {useState} from "react";
+import {useHistory} from "react-router-dom";
 import { withRouter } from "react-router";
 import "./css/GuidePage.css";
 
 function GuidePage() {
-  const [text, setText] = useState("");
-  function onChange(e: any){
-    setText(e.target.value);
-  }
-  function onSubmit(e: any){
-    e.preventDefault();
-    setText("");
-  }
+
+  const history = useHistory();
+
   return (
   <div className="guidePageContainer">
-    <h1>To Do</h1>
-    <form onSubmit={onSubmit}>
-      <input type="text" value={text} onChange={onChange}/>
-      <button>Add</button>
-    </form>
-    <ul></ul>
+    <button onClick={() => history.push("/login")}>로그인</button>
   </div>)
 }
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,10 @@ import { withRouter } from "react-router";
 import "./css/LoginPage.css";
 
 function LoginPage() {
-  return (<div className="loginPageContainer">로그인페이지</div>)
+  return (<div className="loginPageContainer">로그인페이지
+  <button>카카오로그인</button>
+  <button>구글로그인</button>
+  </div>)
 }
 
 export default withRouter(LoginPage);
