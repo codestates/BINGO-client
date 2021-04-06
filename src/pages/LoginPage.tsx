a63c3c06ad5c188dfe57e620f2baa22549223311
JunghoChoi <9rganizedchaos@gmail.com>
2021-04-06 10:19:57 +0900

    Fix GuidePage feature12

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,15 @@
 import { withRouter } from "react-router";
 import "./css/LoginPage.css";
 
+
 function LoginPage() {
+
+  const kakaoLoginHandler = () => {
+    window.location.assign('https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=a7fac194ba87a5958cc6f94e2c2a9cf7&redirect_uri=http://localhost:3000/oauth')
+  }
+
   return (<div className="loginPageContainer">로그인페이지
-  <button>카카오로그인</button>
+  <button onClick={kakaoLoginHandler}>카카오로그인</button>
   <button>구글로그인</button>
   </div>)
 }
--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -2,7 +2,24 @@ import { withRouter } from "react-router";
 import "./css/TestPage.css";
 
 function TestPage() {
-  return (<div className="testPageContainer">테스트페이지</div>)
+  return (<div className="testPageContainer">
+    <div className="testNavPart">
+      <img className="testNavLogo" src="" alt="bingo_logo"/>
+    </div>
+    <div className="testMainPart">
+      <div className="testMainNumber">1/10</div>
+      <div className="testMainQuestion">Q1. 나는 길고양이나 유기견을 보면 마음이 아프다</div>
+      <div className="testMainBtnBox">
+        <button className="testMainYesBtn">Yes</button>
+        <button className="testMainNoBtn">No</button>
+      </div>
+    </div>
+    <div className="testExtraPart">
+      <div className="testExtraDescription">위 열 가지 질문에 답해주시면, BINGO가 당신의 후원유형을 분석해드립니다</div>
+      <button className="testExtraSubmitBtn">submit</button>
+    </div>
+    <button className="testSkipBtn">Skip</button>
+  </div>)
 }
 
 export default withRouter(TestPage);
