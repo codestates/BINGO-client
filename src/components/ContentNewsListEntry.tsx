01ab44d7d74f6339b369f65e0206a349a3fbc602
JunghoChoi <9rganizedchaos@gmail.com>
2021-04-06 14:58:56 +0900

    Add ContentPage into feature10

---
+++ src/components/ContentNewsListEntry.tsx
@@ -0,0 +1,15 @@
+import "./css/ContentNewsListEntry.css"
+
+function ContentNewsListEntry() {
+  return (
+  <div id="contentNewsListEntryContainer">
+    <img src="" alt="news_img" id="contentNewsImg"/>
+    <div id="contentNewsInfo">
+      <div id="contentNewsTitle">동물권 카라 2020년 한 해 유기견 총 1000마리 입양보내다</div>
+      <p id="contentNewsDescription">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus neque, deserunt tenetur dolor delectus ratione inventore exercitationem, eum ad voluptatibus facere nisi odio! Architecto doloremque repellat corporis sapiente, et odit qui illum consectetur hic deleniti minus dignissimos! Possimus aliquam ex qui sequi cumque aspernatur, corrupti quia ducimus aut itaque placeat.</p>
+    </div>
+  </div>
+  )
+}
+
+export default ContentNewsListEntry
--- src/pages/ContentPage.tsx
+++ src/pages/ContentPage.tsx
@@ -1,13 +1,39 @@
 import { withRouter } from "react-router";
 import "./css/ContentPage.css";
+import ContentNewsList from "../components/ContentNewsList"
+import Footer from "../components/Footer"
 
 function ContentPage() {
   return (
   <div id="contentPageContainer">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+    <div id="contentNavPart">
+      <img src="" alt="bingo_logo" />
+      <button>My Page</button>
+    </div>
+    <div id="contentBannerPart">
+      <img id="contentMainBanner" src="" alt="main_banner"/>
+      <button>✨ 관심단체 추가 ✨</button>
+    </div>
+    <div id="contentNGOInfoPart">
+      <img id="contentNGOLogo" src="" alt="ngo_logo"/>
+      <div id="contentNGOInfo">
+        <span>동물권행동 카라</span>
+        <button>후원하기</button>
+      </div>
+    </div>
+    <div id="contentContentPart">
+      <div id="contentMainContent">
+        <video src="" />
+        <div className="contentNGODescription">동물권행동 카라는 동물의 권익을 보호하기 위해 활동하는 동물권 단체이다</div>
+        <div className="contentNGODescription">76명의 BINGO회원이 동물권행동 카라를 후원하고 있습니다.</div>
+        <div className="contentNGODescription">동물권행동 카라는 1999년 김코딩이 설립한 시민단체입니다.</div>
+      </div>
+      <div id="contentSideNews">
+        <span>동물권행동 카라 관련 소식</span>
+        <ContentNewsList />
+      </div>
+    </div>
+    <Footer/>
   </div>
   )
 }
