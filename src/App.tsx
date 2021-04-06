35c9fa0fe1bc9613f2e0c576d5d12fe8ac978dd3
자 <jkyang9118@gmail.com>
2021-04-06 13:39:27 +0900

    Add kakaologin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect }from 'react';
-import {BrowserRouter as Switch, Route} from "react-router-dom"
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Switch, Route, useHistory } from "react-router-dom";
+import "./App.css";
 import GuidePage from "./pages/GuidePage";
 import LoginPage from "./pages/LoginPage";
 import ListPage from "./pages/ListPage";
@@ -8,73 +8,89 @@ import ContentPage from "./pages/ContentPage";
 import MyPage from "./pages/MyPage";
 import PayPage from "./pages/PayPage";
 import TestPage from "./pages/TestPage";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
-
+  const history = useHistory();
   const [isLogin, setIsLogin] = useState(false);
-  const [accessToken, setAccessToken] = useState(''); 
-  
-  const getAccessToken = async (authorizationCode: string) => {
-    console.log('abc')
-    await axios.post('http://localhost:5000/googlelogin', {
-      authorizationCode,
-    })
-    .then(res => {
-      setIsLogin(true)
-      setAccessToken(res.data.accessToken)
-      console.log('abcde')
-    })
-    .catch(err => console.log(err))
-  }
+  const [accessToken, setAccessToken] = useState("");
 
-  const getAccessTokenKakao = async(authorizationCode: string) => {
-    console.log('abc')
-    await axios.post('http://localhost:5000/kakaologin', {
-      authorizationCode,
-    })
-    .then(res => {
-      setIsLogin(true)
-      setAccessToken(res.data.accessToken)
-      console.log('abcde')
-    })
-    .catch(err => console.log(err))
-  }
+  const getAccessToken = async (authorizationCode: string) => {
+    console.log("abc");
+    await axios
+      .post("http://localhost:5000/googlelogin", {
+        authorizationCode,
+      })
+      .then(res => {
+        setIsLogin(true);
+        setAccessToken(res.data.accessToken);
+        console.log("abcde");
+      })
+      .catch(err => console.log(err));
+  };
+
+  const getAccessTokenKakao = async (authorizationCode: string) => {
+    console.log("abc");
+    await axios
+      .post("http://localhost:5000/kakaologin", {
+        authorizationCode,
+      })
+      .then(res => {
+        setIsLogin(true);
+        setAccessToken(res.data.accessToken);
+        console.log("abcde");
+        console.log("res check:", res.data.accessToken);
+        console.log("before:", accessToken);
+      })
+      .catch(err => console.log(err));
+  };
+
+  const kakaoLogout = async () => {
+    await axios
+      .post("http://localhost:5000/kakaologout", {
+        access_token: accessToken,
+      })
+      .then(res => {
+        console.log("로그아웃 res:", res.data);
+      })
+      .catch(err => console.log(err));
+  };
 
   useEffect(() => {
-    if (accessToken === '') {
-      const url = new URL(window.location.href)
-      const authorizationCode = url.searchParams.get('code')
+    if (accessToken === "") {
+      const url = new URL(window.location.href);
+      const authorizationCode = url.searchParams.get("code");
       if (authorizationCode) {
         getAccessToken(authorizationCode);
         getAccessTokenKakao(authorizationCode);
       }
-      console.log(authorizationCode)
+      console.log(authorizationCode);
     }
   });
 
   return (
-    <div className="App">
+    <div className='App'>
+      <button onClick={kakaoLogout}>로그아웃</button>
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <GuidePage />
         </Route>
-        <Route exact path="/login">
+        <Route exact path='/login'>
           <LoginPage />
         </Route>
-        <Route exact path="/test">
+        <Route exact path='/test'>
           <TestPage />
         </Route>
-        <Route exact path="/list">
+        <Route exact path='/list'>
           <ListPage />
         </Route>
-        <Route exact path="/content">
+        <Route exact path='/content'>
           <ContentPage />
         </Route>
-        <Route exact path="/mypage">
+        <Route exact path='/mypage'>
           <MyPage />
         </Route>
-        <Route exact path="/pay">
+        <Route exact path='/pay'>
           <PayPage />
         </Route>
       </Switch>
--- src/pages/GuidePage.tsx
+++ src/pages/GuidePage.tsx
@@ -1,15 +1,15 @@
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { withRouter } from "react-router";
 import "./css/GuidePage.css";
 
 function GuidePage() {
-
   const history = useHistory();
 
   return (
-  <div className="guidePageContainer">
-    <button onClick={() => history.push("/login")}>로그인</button>
-  </div>)
+    <div className='guidePageContainer'>
+      <button onClick={() => history.push("/login")}>로그인</button>
+    </div>
+  );
 }
 
 export default withRouter(GuidePage);
