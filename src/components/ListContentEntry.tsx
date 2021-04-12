caaa44efc301b88cc62bb20fa3950617a92c84ca
JunghoChoi <9rganizedchaos@gmail.com>
2021-04-12 10:09:58 +0900

    Fix Component Setting for Redux Setting

--- src/components/ListContentEntry.tsx
+++ src/components/ListContentEntry.tsx
@@ -1,8 +1,11 @@
 import "./css/ListContentEntry.css";
 
 export default function ListContentEntry() {
+  const handleContentListEntryClick = () => {
+    window.location.href = "./content"
+  }
   return(
-  <div id="ListContentEntryContainer" className="shadow">
+  <div id="ListContentEntryContainer" className="shadow" onClick={handleContentListEntryClick}>
     <img id="ListContentEntryLogo" alt="NGO_logo" src="" />
     <div id="ListContentEntryTitle">동물권행동 카라</div>
     <div id="ListContentEntryDescription">89명의 회원이 후원중</div>
