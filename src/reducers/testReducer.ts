7a7042dff927e488833d5c5198289ca81e5d06fd
성재영 <jaeyeong@MacBookPro.local>
2021-04-13 17:28:32 +0900

    Add feature15

--- src/reducers/testReducer.ts
+++ src/reducers/testReducer.ts
@@ -1,8 +1,18 @@
-import { ANIMATE_TEST } from "../action/index";
+import { ANIMATE_TEST, PUSH_OPTION, CHANGE_OPTION_COLOR } from "../action/index";
 import { initialState } from "./initialState";
 
 const testReducer = (state = initialState, action: any) => {
   switch (action.type) {
+    case PUSH_OPTION:
+      return Object.assign({}, state, {
+        selectedOptions: action.payload.array
+      });
+
+    case CHANGE_OPTION_COLOR:
+      return Object.assign({}, state, {
+        optionList: state.optionList.map((el, i) => i === action.payload.index ? { ...el, color: action.payload.color } : el)
+      });
+
     case ANIMATE_TEST:
       return Object.assign({}, state, {
         testList: state.testList.map((el, i) => i === action.payload.index ? { ...el, left: -100, opacity: 0 } : el)
