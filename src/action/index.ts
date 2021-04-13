63d0ff4ac1e6fcc0b955ccdbfcec412490adc148
자 <jkyang9118@gmail.com>
2021-04-13 17:31:44 +0900

    Fix feature22

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,6 +1,7 @@
 // action types
 export const DO_LOGIN = "DO_LOGIN";
 export const ANIMATE_TEST = "ANIMATE_TEST";
+export const SHOW_LIST = "SHOW_LIST";
 
 // actions creator functions
 export const doLogin = (accessToken: string) => {
@@ -9,15 +10,23 @@ export const doLogin = (accessToken: string) => {
     payload: {
       isLogin: true,
       accessToken,
-    }
-  }
-}
+    },
+  };
+};
 
 export const animateCard = (index: number) => {
   return {
     type: ANIMATE_TEST,
     payload: {
       index,
-    }
-  }
-}
+    },
+  };
+};
+
+export const showList = (id: string) => {
+  return {
+    payload: {
+      id,
+    },
+  };
+};
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,10 +1,13 @@
-import { combineReducers } from 'redux';
-import loginReducer from "./loginReducer"
-import testReducer from "./testReducer"
+import { combineReducers } from "redux";
+import loginReducer from "./loginReducer";
+import testReducer from "./testReducer";
+import listReducer from "./listReducer";
 
 const rootReducer = combineReducers({
-  loginReducer, testReducer,
+  loginReducer,
+  testReducer,
+  listReducer,
 });
 export default rootReducer;
 
-export type RootState = ReturnType<typeof rootReducer>
+export type RootState = ReturnType<typeof rootReducer>;
