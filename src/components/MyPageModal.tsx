3b73540e8bd801f1726fd74e70b8095bb0941c37
JunghoChoi <9rganizedchaos@gmail.com>
2021-04-17 16:54:58 +0900

    Fix Graphs feature28

---
+++ src/components/MyPageModal.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./css/MyPageModal.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../reducers";
+import { showMypageModal } from "../action";
+
+function MyPageModal() {
+
+  const disptach = useDispatch();
+  const state = useSelector((state: RootState) => state.contentReducer);
+
+  const handleClickClose = () => {
+    disptach(showMypageModal(false));
+  }
+
+  const handleRedirectClick = () => {
+    window.location.href = "./pay"
+  }
+
+  return(
+    <>
+    {state.myModalInfo.modalDisplay ? (
+      <div className="modalWholeContainer">
+        <div className="modalContainer shadow">
+          <div className="modalContentPart shadow">
+            <div id="contentModalTitle">{`단체가 MINJE님의 후원결제리스트에 담겼습니다`}</div>
+            <div id="payModalSubtitle2"> 누를시 페이페이지로 이동합니다</div>
+            <div id="contentModalConfirmBtn" className="shadow" onClick={handleRedirectClick}>이동</div>
+          </div>
+          <div className="payPageModalCloseBtn shadow" onClick={handleClickClose}>X</div>
+        </div>
+      </div>
+    ) : null}
+    </>
+  )
+}
+
+export default MyPageModal;
--- src/components/MyRegularDonationListEntry.tsx
+++ src/components/MyRegularDonationListEntry.tsx
@@ -1,13 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../reducers';
 import "./css/MyRegularDonationListEntry.css";
+import { showMypageModal } from "../action"
 
-function MyRegularDonationListEntry(){
+interface MyRegularProps{
+  money: number
+  ngoName: string
+}
+
+function MyRegularDonationListEntry(props: MyRegularProps){
+  const state = useSelector((state: RootState) => state.contentReducer);
+  const dispatch = useDispatch();
+
+  const handleSupportCancelClick = () => {
+    dispatch(showMypageModal(true));
+    console.log("정기후원 해지버튼 클릭")
+  }
+
   return (
   <div id="myRegularDonationListEntryContainer" className="shadow">
     <div>
-      <div id="myRegularDonationNGO">동물권행동 카라</div>
-      <div id="myRegularDonationPrice">10000원</div>
+      <div id="myRegularDonationNGO">{props.ngoName}</div>
+      <div id="myRegularDonationPrice">{`${props.money}원`}</div>
     </div>
-    <button>정기후원 해지</button>
+    <button onClick={handleSupportCancelClick}>정기후원 해지</button>
   </div>
   )
 }
