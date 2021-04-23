db4f5cda9d0142ef4b14671cca9af3c0e9f7a2dc
신민제 <sinminje@sinminje-ui-MacBookPro.local>
2021-04-23 14:52:36 +0900

    Fix css error feature36

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,7 +23,7 @@ function Footer(){
             <animated.div style={scrolling}>
               JAEYEONG SUNG&emsp;<a href="https://github.com/Sungjaeyeong">Move to GitHub</a>&emsp;&emsp;
               MINJE SHIN&emsp;<a href="https://github.com/tlsalswp1004">Move to GitHub</a>&emsp;&emsp;
-              JAKYEONG YANG&emsp;<a href="https://github.com/louis220">Move to GitHub</a>&emsp;&emsp;
+              JAKYUNG YANG&emsp;<a href="https://github.com/louis220">Move to GitHub</a>&emsp;&emsp;
               JUNGHO CHOI&emsp;<a href="https://github.com/9rganizedchaos">Move to GitHub</a>
             </animated.div>
         </div>
